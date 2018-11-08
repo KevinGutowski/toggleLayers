2c669b76151c76643eef9985b42d184ecacd848b
Kevin Gutowski <kgutowski@berkeley.edu>
2018-11-07 20:51:28 -0800

    Add initial version of plugin

--- src/my-command.js
+++ src/my-command.js
@@ -2,5 +2,43 @@ import sketch from 'sketch'
 // documentation: https://developer.sketchapp.com/reference/api/
 
 export default function() {
-  sketch.UI.message("It's alive 🙌")
+  const doc = sketch.getSelectedDocument()
+  const selectedLayers = doc.selectedLayers
+
+  var check = checkLayers(selectedLayers)
+  if (check == 0) {
+    // Turn all selected layers hidden
+    selectedLayers.forEach(function(layer) {
+        layer.hidden = 1
+    })
+  } else if (check == 1) {
+    // Turn all selected layers visible
+    selectedLayers.forEach(function(layer) {
+        layer.hidden = 0
+    })
+  } else if (check == 2) {
+    sketch.UI.message("No selected layers. Please Select a layer.")
+  }
+
+}
+
+function checkLayers(selectedLayers) {
+    var isVisibleCount = []
+    var isHiddenCount = []
+    selectedLayers.forEach(function(layer) {
+        if (layer.hidden == 1) {
+            isHiddenCount.push(true)
+        } else {
+            isVisibleCount.push(false)
+        }
+    })
+    if ((isVisibleCount.length == 0) && (isHiddenCount.length > 0)) {
+        return 1 //All Hidden
+    } else if ((isVisibleCount.length > 0) && (isHiddenCount.length == 0)) {
+        return 0 //All Visible or at least one visible
+    } else if (selectedLayers.length == 0) {
+        return 2 //No layers selected
+    } else {
+        return 0 //At least one layer is visible
+    }
 }
--- togglelayers.sketchplugin/Contents/Sketch/my-command.js
+++ togglelayers.sketchplugin/Contents/Sketch/my-command.js
@@ -105,8 +105,46 @@ __webpack_require__.r(__webpack_exports__);
  // documentation: https://developer.sketchapp.com/reference/api/
 
 /* harmony default export */ __webpack_exports__["default"] = (function () {
-  sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("It's alive 🙌");
+  var doc = sketch__WEBPACK_IMPORTED_MODULE_0___default.a.getSelectedDocument();
+  var selectedLayers = doc.selectedLayers;
+  var check = checkLayers(selectedLayers);
+
+  if (check == 0) {
+    // Turn all selected layers hidden
+    selectedLayers.forEach(function (layer) {
+      layer.hidden = 1;
+    });
+  } else if (check == 1) {
+    // Turn all selected layers visible
+    selectedLayers.forEach(function (layer) {
+      layer.hidden = 0;
+    });
+  } else if (check == 2) {
+    sketch__WEBPACK_IMPORTED_MODULE_0___default.a.UI.message("No selected layers. Please Select a layer.");
+  }
 });
+
+function checkLayers(selectedLayers) {
+  var isVisibleCount = [];
+  var isHiddenCount = [];
+  selectedLayers.forEach(function (layer) {
+    if (layer.hidden == 1) {
+      isHiddenCount.push(true);
+    } else {
+      isVisibleCount.push(false);
+    }
+  });
+
+  if (isVisibleCount.length == 0 && isHiddenCount.length > 0) {
+    return 1; //All Hidden
+  } else if (isVisibleCount.length > 0 && isHiddenCount.length == 0) {
+    return 0; //All Visible or at least one visible
+  } else if (selectedLayers.length == 0) {
+    return 2; //No layers selected
+  } else {
+      return 0; //At least one layer is visible
+    }
+}
 
 /***/ }),
 
